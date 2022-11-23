e53e46f8365add1249f73aa74c12d0f280efdc77
aexarriaga2000 <alexarriaga3000@gmail.com>
2022-11-23 17:22:08 -0600

    Navbar

---
+++ src/componentes/navbar/navbar.js
@@ -0,0 +1,12 @@
+import "./navbar.css"
+
+const Navbar = ()=> {
+    return
+        <nav>
+            <h1> MyApp </h1>
+            <button className = "boton"> boton 1</button>
+            <button> boton 2</button>
+        </nav>
+}
+
+export default Navbar
