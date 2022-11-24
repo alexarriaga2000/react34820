5003eb9cd07b3c9273c8d781e6bd595acd7bde9b
aexarriaga2000 <alexarriaga3000@gmail.com>
2022-11-23 19:11:16 -0600

    navbar v2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './componentes/navbar/navbar';
+
+function App() {
+  return (
+    <div classname="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
--- src/componentes/navbar/navbar.js
+++ src/componentes/navbar/navbar.js
@@ -2,10 +2,10 @@ import "./navbar.css"
 
 const Navbar = ()=> {
     return
-        <nav>
+        <nav className = "navbar">
             <h1> MyApp </h1>
-            <button className = "boton"> boton 1</button>
-            <button> boton 2</button>
+            <button className = "boton">  boton 1</button>
+            <button className = "boton"> boton 2</button>
         </nav>
 }
 
